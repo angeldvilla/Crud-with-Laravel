10a1b7ca7a769f7c58171fb35b37e341b97df83c
angeldvilla <escarlata.2003@gmail.com>
2024-09-03 11:59:15 -0500

    Add new routes for dashboard

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,15 @@ Route::get('/', function () {
 Route::get('/login', [AutenticacionController::class, 'index']);
 Route::get('/registro', [AutenticacionController::class, 'registro']);
 Route::resource('clientes', ClientesController::class);
+
+
+// Rutas del Dashboard
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+})->name('dashboard');
+
+Route::get('admin/clientes', [ClientesController::class, 'index']);
+/* Route::get('admin/envios', [EnviosController::class, 'index']);
+Route::get('admin/usuarios', [UsuariosController::class, 'index']);
+Route::get('admin/empleados', [EmpleadosController::class, 'index']); */
+
