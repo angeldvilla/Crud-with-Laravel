fbad6ed70619656d35627ac87bbda5b36203666f
angeldvilla <escarlata.2003@gmail.com>
2024-09-03 12:00:58 -0500

    Add index for dashboard template

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,17 @@
+@extends('adminlte::page')
+
+@section('title')
+
+@section('content_header')
+<div class="flex justify-center items-center text-center">
+    <h1>Dashboard</h1>
+</div>
+@stop
+
+@section('content')
+
+
+<div class="flex justify-center items-center text-center">
+    <p>Bienvenido al dashboard.</p>
+</div>
+@stop
