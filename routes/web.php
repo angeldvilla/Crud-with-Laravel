2705be690d12f3a0c7e5e83ec673d36cb166b5d8
angeldvilla <escarlata.2003@gmail.com>
2024-09-06 10:43:17 -0500

    config routes

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,29 @@
 
 use App\Http\Controllers\AutenticacionController;
 use App\Http\Controllers\ClientesController;
+use App\Http\Controllers\EmpleadosController;
+use App\Http\Controllers\EnviosController;
+use App\Http\Controllers\UsuariosController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('landingPage');
 });
 
-Route::get('/login', [AutenticacionController::class, 'index']);
-Route::get('/registro', [AutenticacionController::class, 'registro']);
+Route::get('login', [AutenticacionController::class, 'index']);
+Route::get('registro', [AutenticacionController::class, 'registro']);
 Route::resource('clientes', ClientesController::class);
+Route::resource('empleados', EmpleadosController::class);
+Route::resource('usuarios', UsuariosController::class);
+Route::resource('envios', EnviosController::class);
 
 
 // Rutas del Dashboard
-Route::get('/dashboard', function () {
+Route::get('dashboard', function () {
     return view('dashboard.index');
 })->name('dashboard');
 
-Route::get('admin/clientes', [ClientesController::class, 'index']);
-/* Route::get('admin/envios', [EnviosController::class, 'index']);
-Route::get('admin/usuarios', [UsuariosController::class, 'index']);
-Route::get('admin/empleados', [EmpleadosController::class, 'index']); */
-
+Route::get('dashboard/clientes', [ClientesController::class, 'index']);
+Route::get('dashboard/usuarios', [UsuariosController::class, 'index']);
+Route::get('dashboard/empleados', [EmpleadosController::class, 'index']);
+Route::get('dashboard/envios', [EnviosController::class, 'index']);
