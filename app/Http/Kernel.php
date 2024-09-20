c198cfd090f86ed00f2bf2bc94a1d9bef2bf570f
angeldvilla <escarlata.2003@gmail.com>
2024-09-20 16:44:03 -0500

    Add kernel for auth

---
+++ app/Http/Kernel.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+
+    protected $routeMiddleware = [
+        // Otros middlewares
+        'role' => \App\Http\Middleware\CheckRole::class,
+    ];
+
+    protected $middlewareGroups = [
+        'web' => [
+            // Otros middlewares
+            \App\Http\Middleware\CheckActiveSession::class,
+        ],
+    ];
+}
