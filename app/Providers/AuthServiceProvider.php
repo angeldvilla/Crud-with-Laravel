71c76431124a66ec2ab5548ccf4b29bca870cb9c
angeldvilla <escarlata.2003@gmail.com>
2024-09-23 15:39:58 -0500

    Add policies for view renders

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,17 +3,13 @@
 namespace App\Providers;
 
 use App\Models\Usuarios;
-use App\Policies\EmpleadoPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-
+use Illuminate\Support\Facades\Gate;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
      * Las políticas de acceso para los modelos.
      */
-    protected $policies = [
-        Usuarios::class => EmpleadoPolicy::class,
-    ];
 
     /**
      * Registra cualquier servicio de autenticación o autorización.
@@ -21,5 +17,17 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies(); // Registro de las políticas
+
+        Gate::define('view-dashboard', function ($user) {
+            return $user->id_rol == 1 || $user->id_rol == 2;
+        });
+
+        Gate::define('delete-usuarios', function (Usuarios $user) {
+            return $user->id_rol == 1;
+        });
+
+        Gate::define('delete-empleados', function (Usuarios $user) {
+            return $user->id_rol == 1;
+        });
     }
 }
