ed61da245bc2a8edf200ff0bbbaac03209690f31
angeldvilla <escarlata.2003@gmail.com>
2024-09-23 15:42:01 -0500

    Change structure for routes

--- routes/web.php
+++ routes/web.php
@@ -20,21 +20,25 @@ Route::post('logout', [AutenticacionController::class, 'logout'])->name('logout'
 
 // Rutas protegidas del dashboard
 Route::middleware('auth')->group(function () {
-    Route::resource('dashboard/usuarios', UsuariosController::class);
 
-    Route::middleware('auth')->group(function () {
+    // Rutas del dashboard disponibles para roles 1 y 2
+    /* if (auth()->user()->id_rol == 1 || auth()->user()->id_rol == 2) { */
         Route::get('dashboard', function () {
             return view('dashboard.index');
         })->name('dashboard');
 
-        // Rutas del dashboard disponibles para roles 1 y 2
         Route::resource('dashboard/clientes', ClientesController::class);
         Route::resource('dashboard/empleados', EmpleadosController::class);
         Route::resource('dashboard/envios', EnviosController::class);
-    });
-});
+/*     } */
 
-// Rutas protegidas del cliente
-Route::middleware('auth')->group(function () {
-    Route::get('home', [ClientesController::class, 'home'])->name('home');
+    // Rutas del dashboard disponibles para roles 1 
+  /*   if (auth()->user()->id_rol == 1) { */
+        Route::resource('dashboard/usuarios', UsuariosController::class);
+    /* } */
+
+    // Rutas protegidas del cliente
+  /*   if (auth()->user()->id_rol == 3) { */
+        Route::get('home', [ClientesController::class, 'home'])->name('home');
+    /* } */
 });
