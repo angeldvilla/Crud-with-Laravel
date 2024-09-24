5a845747f9fcac21e772daf81a2e1421256d5b35
angeldvilla <escarlata.2003@gmail.com>
2024-09-24 14:56:28 -0500

    Add functions for auth roles

--- app/Models/Usuarios.php
+++ app/Models/Usuarios.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Str;
 class Usuarios extends Authenticatable
 {
     use HasFactory;
-    
+
     protected $keyType = 'string';
     public $incrementing = false;
 
@@ -33,5 +33,20 @@ class Usuarios extends Authenticatable
                 $model->id = (string) Str::uuid();
             }
         });
+    }
+
+    public function esCliente()
+    {
+        return $this->id_rol === 3; // 3 para clientes
+    }
+
+    public function esEmpleado()
+    {
+        return $this->id_rol === 2; // 2 para empleados
+    }
+
+    public function esAdmin()
+    {
+        return $this->id_rol === 1; // 1 para administradores
     }
 }
